dc8bffb5e6ab5dcf702654165797d4c60348d195
Roman Toritsyn <devroman@mlsworldwide.com>
2023-09-21 17:22:28 +0300

    fix: button text styles

--- src/Elements/PrimaryButton/PrimaryButton.tsx
+++ src/Elements/PrimaryButton/PrimaryButton.tsx
@@ -11,16 +11,15 @@ const PrimaryButton: React.FC<TPrimaryButton> = ({
   name,
   type = "button",
 }) => {
-
   return (
     <button
       type={type}
       aria-label={name}
       className="bg-slate-950 hover:bg-slate-900 p-3 text-xl text-white"
-      style={{border: "2px solid red"}}
+      style={{ border: "2px solid red" }}
       onClick={clickHandler}
     >
-      {name}
+      <span className="text-white">{name}</span>
     </button>
   );
 };
